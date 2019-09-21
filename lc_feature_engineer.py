e1a83bb6289cde40ded915c7979ef06ebfb52c09
AlexHsia010 <46300668+AlexHsia010@users.noreply.github.com>
2019-09-20 20:14:42 -0400

    Update lc_feature_engineer.py

--- lc_feature_engineer.py
+++ lc_feature_engineer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-# Authors: Eric@FLAGDream <eric.d@flagdream.com>
+
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
